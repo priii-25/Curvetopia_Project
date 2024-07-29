a7f40dbfb14a5b57840a520d0c14a89755958063
priii-25 <121582683+priii-25@users.noreply.github.com>
2024-07-29 18:07:46 +0530

    cairo issues solved

--- src/data_preprocessing/__init__.py
+++ src/data_preprocessing/__init__.py
@@ -1,7 +1,8 @@
-from data_preprocessing.preprocess import read_csv, plot
+from data_preprocessing.preprocess import read_csv, plot, polylines2svg
 
 csv_path = 'src/data_preprocessing/isolated.csv'
-svg_path = 'path/to/your/output.svg'
+svg_path = 'src/data_preprocessing/isolated.svg'
 
 path_XYs = read_csv(csv_path)
-plot(path_XYs)
+#plot(path_XYs)
+polylines2svg(csv_path,svg_path)
--- src/data_preprocessing/preprocess.py
+++ src/data_preprocessing/preprocess.py
@@ -1,9 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import svgwrite
-from PIL import Image
-import io
-import tempfile
+import os
+import ctypes
+
+cairo_bin_path = r'D:\msys64\mingw64\bin'
+os.environ['PATH'] = cairo_bin_path + ';' + os.environ['PATH']
+
+try:
+    cairo_lib = ctypes.CDLL(r'D:\msys64\mingw64\bin\libcairo-2.dll')
+    print("Cairo library loaded successfully.")
+except OSError as e:
+    print(f"Failed to load Cairo library: {e}")
+
+import cairosvg
 
 def read_csv(csv_path):
     np_path_XYs = np.genfromtxt(csv_path, delimiter=',')
@@ -25,3 +35,33 @@ def plot(path_XYs, colours=['r', 'g', 'b', 'c', 'm', 'y', 'k']):
             ax.plot(XY[:, 0], XY[:, 1], c=c, linewidth=2)
     ax.set_aspect('equal')
     plt.show()
+
+def polylines2svg(paths_XYs, svg_path, colours=['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'black']):
+    W, H = 0, 0
+    for path_XYs in paths_XYs:
+        for XY in path_XYs:
+            W, H = max(W, np.max(XY[:, 0])), max(H, np.max(XY[:, 1]))
+    
+    padding = 0.1
+    W, H = int(W + padding * W), int(H + padding * H)
+    
+    dwg = svgwrite.Drawing(svg_path, profile='tiny', shape_rendering='crispEdges')
+    group = dwg.g()
+    
+    for i, path in enumerate(paths_XYs):
+        path_data = []
+        c = colours[i % len(colours)]
+        for XY in path:
+            path_data.append(("M", (XY[0, 0], XY[0, 1])))
+            for j in range(1, len(XY)):
+                path_data.append(("L", (XY[j, 0], XY[j, 1])))
+            if not np.allclose(XY[0], XY[-1]):
+                path_data.append(("Z", None))
+        group.add(dwg.path(d=path_data, fill=c, stroke='none', stroke_width=2))
+    
+    dwg.add(group)
+    dwg.save()
+    
+    png_path = svg_path.replace('.svg', '.png')
+    fact = max(1, 1024 // min(H, W))
+    cairosvg.svg2png(url=svg_path, write_to=png_path, parent_width=W, parent_height=H, output_width=fact * W, output_height=fact * H, background_color='white')
