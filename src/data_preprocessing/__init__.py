2d5d63efd26782d4c7fefcb818f8270669ae0c96
priii-25 <121582683+priii-25@users.noreply.github.com>
2024-07-29 16:58:47 +0530

    Update

--- src/data_preprocessing/__init__.py
+++ src/data_preprocessing/__init__.py
@@ -1,8 +1,7 @@
-from data_preprocessing.preprocess import read_csv, polylines2svg, plot
+from data_preprocessing.preprocess import read_csv, plot
 
 csv_path = 'src/data_preprocessing/isolated.csv'
 svg_path = 'path/to/your/output.svg'
 
 path_XYs = read_csv(csv_path)
 plot(path_XYs)
-# polylines2svg(path_XYs, svg_path)
