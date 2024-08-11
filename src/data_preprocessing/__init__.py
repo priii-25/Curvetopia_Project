75535f885b8bae60d0971294d79d67922752f934
priii-25 <121582683+priii-25@users.noreply.github.com>
2024-08-11 22:39:56 +0530

    documented

--- src/data_preprocessing/__init__.py
+++
@@ -1,8 +0,0 @@
-from data_preprocessing.preprocess import read_csv, plot, polylines2svg
-
-csv_path = 'src\curve_completion\occlusion1_sol.csv'
-svg_path = 'src/data_preprocessing/isolated.svg'
-
-path_XYs = read_csv(csv_path)
-#plot(path_XYs)
-polylines2svg(csv_path,svg_path)
--- src/main.py
+++
@@ -1,4 +0,0 @@
-from data_preprocessing import read_csv, convert_to_polylines
-
-data = read_csv("data/raw/example.csv")
-polylines = convert_to_polylines(data)
